28f4b774302396076071565eacc2ca826b22c286
Miguel <miguel.meki@gmail.com>
2026-03-22 19:27:34 +0100

    Update sigpac.js
    fix api/sigpac use CommonJS for Vercel compatibility

--- api/sigpac.js
+++ api/sigpac.js
@@ -1,9 +1,9 @@
-import fetch from 'node-fetch'
-import https from 'https'
+const https = require('https')
+const fetch = require('node-fetch')
 
 const agent = new https.Agent({ rejectUnauthorized: false })
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET')
 
